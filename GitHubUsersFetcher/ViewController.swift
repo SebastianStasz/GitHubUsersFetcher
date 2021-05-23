331d48a208607c8182f558ec96260842dfaecff6
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-05-23 12:29:26 +0200

    Initial Commit

---
+++ GitHubUsersFetcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUsersFetcher
+//
+//  Created by Sebastian Staszczyk on 23/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
